a2d7a6e705bea409fb645c8d5271d35a15e7503e
Bharath <shettybharath004@gmail.com>
2026-09-19 13:42:00 +0530

    my latest commit

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://voice-ai-assistant.vercel.app",
+  "https://voice-ai-asssistant.vercel.app",
 ];
 
 app.use(
